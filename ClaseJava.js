1ee31ca4ca58cfeb3930f3d8ebbc6ec965a2e1a5
Gera <geragt90@gmail.com>
2025-03-25 21:18:10 -0600

    Clase del 25 de Marzo

---
+++ ClaseJava.js
@@ -0,0 +1,113 @@
+/*Características
+•	Lenguaje de Alto Nivel.
+•	Interpretado.
+•	Dinámico.
+•	Débilmente Tipado.
+•	Multi paradigma.
+•	Sensible a MAYÚSCULAS y minúsculas.
+•	No necesitas los puntos y comas al final de cada línea;
+*/
+
+
+/*Escritura de código
+
+Los identificadores deben comenzar con:
+•	Una letra o
+•	Un signo de dolar $ o
+•	Un guión bajo _
+•	Nunca con números o caracteres especiales.
+*/
+
+/*
+Usa snake_case en:
+•	Nombre de archivos:
+mi_archivo_javascript.js;
+
+
+Usa UPPER_CASE en:
+
+•	Constantes:
+const UNA_CONSTANTE = "Soy una constante",
+  PI = 3.141592653589793;
+
+
+Usa UpperCamelCase en:
+
+•	Clases:
+class SerHumano {
+  constructor(nombre, genero) {
+    this.nombre = nombre;
+    this.genero = genero;
+  }
+
+  miNombreEs() {
+    return `Mi nombre es ${this.nombre}`;
+  }
+}
+
+Usa lowerCamelCase en:
+
+•	Objetos:
+const unObjeto = {
+  nombre: "rené",
+  email: "rene.ceron@gmail.com",
+};
+
+•	Primitivos:
+let unaCadena = "Hola Mundo",
+  unNumero = 19,
+  unBoolean = true;
+
+•	Funciones:
+function holaMundo(nombre) {
+  alert(`Hola mundo ${nombre}`);
+}
+holaMundo("Rene");
+
+•	Instancias:
+const ajax = new XMLHttpRequest(),
+  rene = new SerHumano("René", "Hombre");
+
+
+Palabras reservadas
+A: abstract
+B: boolean, break, byte
+C: case, catch, char, class, const, continue
+D: debugger, default, delete, do, double
+E: else, enum, export, extends
+F: false, final, finally, float, for, function
+G: goto
+I: if, implements, import, in, instanceof, int, interface
+L: let, long
+N: native, new, null
+P: package, private, protected, public
+R: return
+S: short, static, super, switch, synchronized
+T: this, throw, throws, transient, true, try, typeof
+V: var, volatile, void
+W: while, with
+
+
+Ordenamiento de código
+1.	IMPORTACIÓN DE MÓDULOS.
+2.	DECLARACIÓN DE VARIABLES.
+3.	DECLARACIÓN DE FUNCIONES.
+4.	EJECUCIÓN DE CÓDIGO.
+
+Tipos de datos en JavaScript
+Primitivos: Se accede directamente al valor.
+•	string
+•	number
+•	boolean
+•	null
+•	undefined
+•	NaN
+
+Compuestos: Se accede a la referencia del valor.
+•	object = {}
+•	array = []
+•	function () { }
+•	Class {}
+•	etc.
+
+*/
--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 alert("Cuidado prro")
 console.log("Este codigo es secreto")
+let clave=123456
+console.log(typeof clave)
